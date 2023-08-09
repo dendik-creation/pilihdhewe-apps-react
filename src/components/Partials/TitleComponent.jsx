5465a0bbd0a8807ee028392480d07afa063ba07f
dendik-creation <setiawandendik0205@gmail.com>
2023-08-09 23:50:11 +0700

    Fix Path File

--- src/components/Partials/TitleComponent.jsx
+++ src/components/Partials/TitleComponent.jsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import IsDefault from "./TitleCheck/isDefault";
+import { useLocation } from "react-router-dom";
 
 export default function TitleComponent() {
+  const location = useLocation();
+  const currentPath = location.pathname;
   return (
     <>
-      <Routes>
-        <Route path="*" element={<IsDefault />} />
-      </Routes>
+      <header className="bg-white shadow-sm">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 flex justify-between items-center">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            {currentPath == "/"
+              ? "Dashboard"
+              : currentPath == "/events"
+              ? "Events"
+              : currentPath == "/siswa"
+              ? "Siswa"
+              : currentPath == "/events/create"
+              ? "Event Baru"
+              : currentPath == "/my-profile"
+              ? "My Profile"
+              : currentPath == "/laporan"
+              ? "Laporan"
+              : currentPath == "/about"
+              ? "About Application"
+              : ""}
+          </h1>
+        </div>
+      </header>
     </>
   );
 }
