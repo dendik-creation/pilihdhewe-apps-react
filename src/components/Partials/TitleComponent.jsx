0d6ccb9f21777267c5ac69934e151dc8975a598d
dendik-creation <setiawandendik0205@gmail.com>
2023-08-09 23:39:53 +0700

    Edit Header Page

--- src/components/Partials/TitleComponent.jsx
+++ src/components/Partials/TitleComponent.jsx
@@ -1,6 +1,4 @@
-import { UserPlusIcon } from "@heroicons/react/24/outline";
 import React from "react";
-import { NavLink, useLocation, useParams } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import IsDefault from "./TitleCheck/isDefault";
 
